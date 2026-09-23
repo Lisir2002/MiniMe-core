3fe8c31cb4b13b58e51d6b21212723058788a6fd
MiniMe Dev <dev@minime.local>
2026-09-24 00:36:49 +0800

    chore(logviewer-app): 版本号 0.0.2

--- logviewer-app/build.gradle.kts
+++ logviewer-app/build.gradle.kts
@@ -56,8 +56,8 @@ android {
         applicationId = "com.mini.logs"
         minSdk = 26
         targetSdk = 28
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "0.0.2"
         vectorDrawables { useSupportLibrary = true }
     }
 
